ea20b7b10a1f112bdaca4ba0b5c185d17c18b942
Kwangsun Noh <nohkwangsun@gmail.com>
2022-10-09 02:04:34 +0800

    add volume to save database

--- bin/docker-restart.sh
+++ bin/docker-restart.sh
@@ -2,4 +2,4 @@
 
 docker pull ghcr.io/nohkwangsun/bookstore:main
 docker rm -f bookstore
-docker run -d -p10000:10000 --name bookstore --pull never ghcr.io/nohkwangsun/bookstore:main
+docker run -d -p10000:10000 --name bookstore -v database:/bookstore/database --pull never ghcr.io/nohkwangsun/bookstore:main
